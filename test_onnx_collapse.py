bdbf19c79edb771f377ab797cdb5bec46103d398
aadams305 <197906464+aadams305@users.noreply.github.com>
2026-04-17 23:58:57 -0400

    test

--- test_onnx_collapse.py
+++
@@ -1,8 +0,0 @@
-import onnxruntime as ort
-import numpy as np
-sess = ort.InferenceSession("models/hand_simcc.onnx", providers=["CPUExecutionProvider"])
-in_name = sess.get_inputs()[0].name
-for i in range(3):
-    img = np.random.randn(1,3,160,160).astype(np.float32) * (i+1)
-    outx, outy = sess.run(None, {in_name: img})
-    print(i, outx[0,0,:5]) # print first 5 bins of joint 0

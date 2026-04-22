b0e81a61cf00e7cad7d4e913e27803d9837c22d5
aadams305 <197906464+aadams305@users.noreply.github.com>
2026-04-22 06:46:08 -0400

    as

--- handtracking/__init__.py
+++ handtracking/__init__.py
@@ -1,13 +1,13 @@
-"""High-FPS 10-point hand tracking (MobileNetV4 + SimCC)."""
+"""High-FPS 21-point hand tracking (MobileNetV4 + SimCC)."""
 
 from handtracking.topology import (
-    HAND_10_NAMES,
+    HAND_21_NAMES,
     MEDIAPIPE_TO_SLOT,
-    mediapipe_indices_10,
+    mediapipe_indices_21,
 )
 
 __all__ = [
-    "HAND_10_NAMES",
+    "HAND_21_NAMES",
     "MEDIAPIPE_TO_SLOT",
-    "mediapipe_indices_10",
+    "mediapipe_indices_21",
 ]
